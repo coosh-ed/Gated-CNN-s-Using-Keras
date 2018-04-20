e93deacad75d670bac9ddbc8c22579c43ed51014
Colm O'Shaughnessy <colmo@Colms-mac.local>
2018-04-20 11:27:45 +0100

    Implemented GCNN

--- GCNN.py
+++ GCNN.py
@@ -1,24 +1,65 @@
 from __future__ import division, print_function, absolute_import
-from keras.layers import Conv1d, ZeroPadding1D
+from keras import backend as K, optimizers
+from keras.engine.topology import Layer, Input
+from keras.layers import Conv1D, ZeroPadding1D, Multiply
+from keras.models import Model
 
-class GConv(Layer):
-    def __init__(self, output_dim, kernel_size, padding="prepend", **kwargs):
+import numpy as np
+
+class GatedConv1D:
+    """Keras-style class implementation of a Gated CNN layer.
+    See https://arxiv.org/abs/1612.08083 for more information.
+    Example:    
+        inp = Input(shape=(None, 9), ...)
+        outp = GatedConv1D(output_dim=5, kernel_size=3)(inp)
+
+    # Arguments
+        ouput_dim (int)
+        kernel_size (int)
+        kwargs_conv (dict) : dictionary of keyword args for Keras Conv1D layer
+        kwargs_gate (dict) : dictionary of keyword args for gated Keras Conv1D layer
+    """
+
+    def __init__(self, output_dim, kernel_size, kwargs_conv={}, kwargs_gate={}):
+        self.conv = Conv1D(output_dim, kernel_size, **kwargs_conv) 
+        self.conv_gate = Conv1D(output_dim, kernel_size, activation="sigmoid", **kwargs_gate)
+        self.pad_input = ZeroPadding1D(padding=(kernel_size-1, 0))
+    
+    def __call__(self, inputs):
+        X = self.pad_input(inputs)
+        A = self.conv(X)
+        B = self.conv_gate(X)
+        return Multiply()([A, B])
+
+
+import unittest
+
+class TestGConv(unittest.TestCase):
+    def setUp(self):
+        kernel = 2
+        input_shape = (1, 4, 6)
+        output_dim = 3
+        test_data = np.zeros(input_shape)
+        inp = Input(shape=(None, input_shape[2]), name="input1")
+        x = GatedConv1D(output_dim, kernel_size=kernel)(inp) 
+        
+        self.sample_zero_model = Model(inputs=inp, outputs=x)
+        self.test_data = test_data
         self.output_dim = output_dim
-        self.pad_input = ZeroPadding1d(padding=(kernel_size-1,))
-        self.conv = Conv1D(output_dim, kernel_size) 
-        self.conv_gate = Conv1(output_dim, kernel_size, activation="sigmoid")
 
-        super(GConv, self).__init__(**kwargs)
+    def test_forward_pass(self):
+        model = self.sample_zero_model
 
-    def build(self, input_shape):
-        self.conv_weights = self.conv.get_weights()
-        self.conv_gate_weights = self.conv_gate.get_weights()
+        sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='mean_squared_error', optimizer=sgd)
+        forward = model.predict(self.test_data, batch_size=None, verbose=1, steps=1)
+        self.assertFalse(forward.any())
 
-    def call(self, x):
-        x = self.pad_input(x)
-        A = self.conv(x)
-        B = self.conv_gate(x)
-        return A.multiply(B)
+        model.fit(self.test_data, np.full(forward.shape, 1), epochs=10)
+        forward = model.predict(self.test_data, batch_size=None, verbose=0, steps=1)
+        self.assertTrue(forward.all())
 
-    def compute_output_shape(self, input_shape):
-        return self.conv.output_shape
+
+if __name__ == "__main__":
+    unittest.main()
+
